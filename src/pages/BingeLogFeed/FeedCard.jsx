8adaf5f65cb7fb21de92a54b07059a5cce0b62f9
WebDev955 <austingeisinger955@gmail.com>
2025-09-27 23:42:21 -0400

    commented out the friendsList and feedcard for now

--- src/pages/BingeLogFeed/FeedCard.jsx
+++ src/pages/BingeLogFeed/FeedCard.jsx
@@ -1,45 +1,45 @@
 //IMPORTS - Hooks
-import { useContext} from "react"
+//import { useContext} from "react"
 //IMPORTS - Components 
-import { UserProfileContext } from "../../components/Contexts/UserProfileContext"
-import { UserAccountContext } from "../../components/Contexts/UserAccountContext"
+//import { UserProfileContext } from "../../components/Contexts/UserProfileContext"
+//import { UserAccountContext } from "../../components/Contexts/UserAccountContext"
 //IMPORTS - Styles
 
-import styles from "./FeedCard.module.css"
+//import styles from "./FeedCard.module.css"
 
-function FeedCard() {
+//function FeedCard() {
 
-const userProfile = useContext(UserProfileContext)
-const userAcct = useContext(UserAccountContext)
+//const userProfile = useContext(UserProfileContext)
+//const userAcct = useContext(UserAccountContext)
 
     // Best Option: Add a conditional check here
     //if (!userAcct.friendsList || userAcct.friendsList.length === 0) {
        // return <p>No friends to display yet. Add some!</p>;
   //  }
 
-  return (
-  <>
-    {userAcct.bingeFeed.map((friend) => (
-      <main className = {styles.mainWrapper}>
-        <article>
-          <header className={styles.header}>
-            <img src={userProfile.bioAvatar} width="40" height="40"/>
-            <p>{friend.userName}</p>
-          </header>
-          <div>
-            <p>Currently Binging: {friend.currBinge}</p>
-              {friend.recentlyWatchedEp.map((ep) => (
-                <p>Recently Watched: "{ep.epName}" - {ep.showName} </p>
-              ))}
-            <p>Recently Finished: {friend.recentlyFinishedShow}</p>
-          </div>
-        </article>
-        <div className={styles.actionBar}>
-          <p>Action Bar</p>
-        </div>
-      </main>
-    ))}
-  </>
+  //return (
+ // <>
+   // {userAcct.bingeFeed.map((friend) => (
+    //  <main className = {styles.mainWrapper}>
+     //   <article>
+       ///   <header className={styles.header}>
+           // <img src={userProfile.bioAvatar} width="40" height="40"/>
+           // <p>{friend.userName}</p>
+         /// </header>
+        //  <div>
+         //   <p>Currently Binging: {friend.currBinge}</p>
+             // {friend.recentlyWatchedEp.map((ep) => (
+             //   <p>Recently Watched: "{ep.epName}" - {ep.showName} </p>
+             // ))}
+           // <p>Recently Finished: {friend.recentlyFinishedShow}</p>
+         // </div>
+       // </article>
+      //  <div className={styles.actionBar}>
+         // <p>Action Bar</p>
+      //  </div>
+     // </main>
+   // ))}
+ // </>
 
   )
 }
--- src/pages/FriendsList/FriendsList.jsx
+++ src/pages/FriendsList/FriendsList.jsx
@@ -1,35 +1,35 @@
 //IMPORTS - Hooks
-import { useContext, useEffect, useState } from "react"
+//import { useContext, useEffect, useState } from "react"
 //IMPORTS - Components 
-import{ UserAccountContext} from "../../components/Contexts/UserAccountContext"
+//import{ UserAccountContext} from "../../components/Contexts/UserAccountContext"
 
 //IMPORTS - Styles
 //import styles from "./UserPage.module.css"
 
 
-function FriendsList() {
+//function FriendsList() {
 
-const userAcct = useContext(UserAccountContext)
-const [globalUsers, setGlobalUsers] = useState([])
+//const userAcct = useContext(UserAccountContext)
+//const [globalUsers, setGlobalUsers] = useState([])
 
-useEffect(() => {
-  fetch('http://localhost:3000/users')
-    .then (res => res.json())
-    .then (data => setGlobalUsers(data))
-},[])
+//useEffect(() => {
+ // fetch('http://localhost:3000/users')
+   // .then (res => res.json())
+   // .then (data => setGlobalUsers(data))
+//},[])
 
- return (
-    <>
-      <h1>Friends List</h1>
-      {userAcct.userAccount.friendList.map(friendId => {
-        const friend = globalUsers.find(user => user.id === friendId);
-        return (
-          <div key={friendId}>
-            <p>{friend ? friend.userName : "Unknown User"}</p>
-          </div>
-        );
-      })}
-    </>
-  );
-}
-export default FriendsList
+ //return (
+   // <>
+     // <h1>Friends List</h1>
+     // {userAcct.userAccount.friendList.map(friendId => {
+       /// const friend = globalUsers.find(user => user.id === friendId);
+       // return (
+        //  <div key={friendId}>
+          //  <p>{friend ? friend.userName : "Unknown User"}</p>
+       /  // </div>
+       // );
+      //})}
+    //</>
+  //);
+//}
+//export default FriendsList
